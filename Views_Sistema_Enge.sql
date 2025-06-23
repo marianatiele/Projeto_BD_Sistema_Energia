95ada8106c1f2d400de3a994bc7335daa435f137
Maria Natiele <106673906+marianatiele@users.noreply.github.com>
2025-06-23 17:21:45 -0300

    Update Views_Sistema_Enge.sql

--- Views_Sistema_Enge.sql
+++ Views_Sistema_Enge.sql
@@ -1,6 +1,6 @@
 
 -- localização e data das medições
-create or replace view localizacao_cliente as
+create or replace view sh_enge.localizacao_cliente as
 select cf.cod_fatura, c.nome, cf.data_medicao, cf.localizacao, c.cidade, c.bairro,c.rua, c.num_casa
 from sh_enge.consumir cm join sh_enge.consumo_faturas cf on cf.cod_consumo = cm.cod_consumo
 join sh_enge.cliente c on c.cod_cliente = cm.cod_cliente;
@@ -10,7 +10,7 @@ select * from localizacao_cliente;
 -- Materizalizad View
 
 
-create materialized view  if not exists fatura_status
+create materialized view  if not exists sh_enge.fatura_status
    as   
    select c.nome,c.email, c.cod_cliente, cf.kw, cf.data_venciemnto, p.valor_pago, p.status_pagamento
    from sh_enge.cliente c join sh_enge.consumir s 
