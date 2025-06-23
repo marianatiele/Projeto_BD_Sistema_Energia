89caac23825bf69bc554f1b806b1b63a1b13260c
Maria Natiele <106673906+marianatiele@users.noreply.github.com>
2025-06-23 15:25:42 -0300

    Update Views_Sistema_Enge.sql

--- Views_Sistema_Enge.sql
+++ Views_Sistema_Enge.sql
@@ -2,8 +2,8 @@
 -- localização e data das medições
 create or replace view localizacao_cliente as
 select cf.cod_fatura, c.nome, cf.data_medicao, cf.localizacao, c.cidade, c.bairro,c.rua, c.num_casa
-from consumir cm join consumo_faturas cf on cf.cod_consumo = cm.cod_consumo
-join cliente c on c.cod_cliente = cm.cod_cliente;
+from sh_enge.consumir cm join sh_enge.consumo_faturas cf on cf.cod_consumo = cm.cod_consumo
+join sh_enge.cliente c on c.cod_cliente = cm.cod_cliente;
 
 select * from localizacao_cliente;
 
@@ -13,10 +13,10 @@ select * from localizacao_cliente;
 create materialized view  if not exists fatura_status
    as   
    select c.nome,c.email, c.cod_cliente, cf.kw, cf.data_venciemnto, p.valor_pago, p.status_pagamento
-   from cliente c join consumir s 
+   from sh_enge.cliente c join sh_enge.consumir s 
    on s.cod_cliente  = c.cod_cliente 
-   join consumo_faturas cf on cf.cod_consumo = s.cod_consumo
-   join pagamento p on p.cod_fatura  = s.cod_fatura; 
+   join sh_enge.consumo_faturas cf on cf.cod_consumo = s.cod_consumo
+   join sh_enge.pagamento p on p.cod_fatura  = s.cod_fatura; 
 
 
 
