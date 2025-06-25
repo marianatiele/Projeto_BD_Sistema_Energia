001806b512d7154683162d82c21dcde8e9995935
Maria Natiele <106673906+marianatiele@users.noreply.github.com>
2025-06-24 21:35:27 -0300

    Update Views_Sistema_Enge.sql

--- Views_Sistema_Enge.sql
+++ Views_Sistema_Enge.sql
@@ -1,26 +1,35 @@
+-- Views - Cliente e tipo de medidor com data de instalação 
 
--- localização e data das medições
-create or replace view sh_enge.localizacao_cliente as
-select cf.cod_fatura, c.nome, cf.data_medicao, cf.localizacao, c.cidade, c.bairro,c.rua, c.num_casa
-from sh_enge.consumir cm join sh_enge.consumo_faturas cf on cf.cod_consumo = cm.cod_consumo
-join sh_enge.cliente c on c.cod_cliente = cm.cod_cliente;
+create or replace view sm_enge.vw_cliente_medidor as
+select c.nome, c.telefone, c.cidade, c.bairro, md.data_instalacao, md.tipo
+from sm_enge.Cliente c join sm_enge.Medidor md on c.id_cliente = md.id_cliente
 
-select * from localizacao_cliente;
+
+select * from sm_enge.vw_cliente_medidor;
+
+-- Views - Pagamento de Fatura por tipo de pagamento
+
+
+create or replace view sm_enge.contas_pagas_status_pagamento
+as 
+select cl.nome, cl.cidade,  p.data_pagamento, p.forma_pagamento, m.kw,  p.valor_pago, cn.status
+from sm_enge.Conta cn join sm_enge.Pagamento p on p.id_conta = cn.id_conta
+join sm_enge.medidor m on m.id_medidor = cn.id_medidor
+join sm_enge.Cliente cl on cl.id_cliente = m.id_cliente;
+
+select * from sm_enge.contas_pagas_status_pagamento;
 
 -- Materizalizad View
+-- Contas que os atendente emitiram e seus respectivos valores
 
-
-create materialized view  if not exists sh_enge.fatura_status
+create materialized view  if not exists sm_enge.fun_emissao_conta
    as   
-   select c.nome,c.email, c.cod_cliente, cf.kw, cf.data_venciemnto, p.valor_pago, p.status_pagamento
-   from sh_enge.cliente c join sh_enge.consumir s 
-   on s.cod_cliente  = c.cod_cliente 
-   join sh_enge.consumo_faturas cf on cf.cod_consumo = s.cod_consumo
-   join sh_enge.pagamento p on p.cod_fatura  = s.cod_fatura; 
+   select f.nome, f.cargo, c.data_emissao, c.valor_total
+   from sm_enge.Funcionario f join sm_enge.Conta c
+   on f.id_funcionario = c.id_funcionario;
+ 
 
-
-
-select * from fatura_status;
+select * from sm_enge.fun_emissao_conta;
 
 
 
